8a62a0fa663999a40af08a4626f6461246141ad2
Dialpuri <dialpuri.jordan@gmail.com>
2022-12-31 11:02:29 +0000

    Initial commit

---
+++ site/src/routes/+layout.svelte
@@ -0,0 +1,173 @@
+<!--<nav class="navbar fixed-top shadow-sm navbar-expand-lg bg-light navbar-light py-3 py-lg-0 px-lg-5">-->
+<!--    <a href="index.html" class="navbar-brand ml-lg-3">-->
+<!--        <h1 class="m-0 display-5"><span class="text-primary">Jordan </span>Dialpuri</h1>-->
+<!--    </a>-->
+<!--    <button type="button" class="navbar-toggler" data-toggle="collapse" data-target="#navbarCollapse">-->
+<!--        <span class="navbar-toggler-icon"></span>-->
+<!--    </button>-->
+<!--    <div class="collapse navbar-collapse px-lg-3" id="navbarCollapse">-->
+<!--        <div class="navbar-nav m-auto py-0">-->
+<!--            <a href="#home" class="nav-item nav-link active">Home</a>-->
+<!--            <a href="#about" class="nav-item nav-link">About</a>-->
+<!--            <a href="#projects" class="nav-item nav-link">Projects</a>-->
+<!--            <a href="#contact" class="nav-item nav-link">Contact</a>-->
+<!--            <a href = "https://www.github.com/dialpuri"><img style = "max-height: 2rem;" src = "images/github.png"></a>-->
+<!--            <a href = "https://www.linkedin.com/in/jordan-dialpuri/"><img style = "max-height: 2rem;" src = "images/linkedin.png"></a>-->
+
+<!--        </div>-->
+<!--    </div>-->
+<!--</nav>-->
+<!-- Navbar End -->
+
+
+<!--<script>-->
+<!--import logo from '$lib/static/favicon.png'-->
+<!--</script>-->
+
+<div class = "navbar_">
+
+    <h1 style=""><span class="text-primary">Jordan </span>Dialpuri</h1>
+
+
+    <ul class = "navbar-links">
+
+        <input type="checkbox" id="checkbox_toggle" />
+
+        <label for="checkbox_toggle" class="hamburger">&#9776;</label>
+
+        <div class = "menu">
+            <li>
+                <a href = "/">
+                    <span>Home</span>
+                </a>
+            </li>
+            <li>
+                <a href = "#about">
+                    <span>About</span>
+                </a>
+            </li>
+            <li>
+                <a href = "#projects">
+                    <span>Projects</span>
+                </a>
+            </li>
+            <li>
+                <a href = "#contact">
+                    <span>Contact</span>
+                </a>
+            </li>
+            <a href = "https://www.github.com/dialpuri"><img style = "max-height: 2rem;" src = "images/github.png"></a>
+            <a href = "https://www.linkedin.com/in/jordan-dialpuri/"><img style = "max-height: 2rem;" src = "images/linkedin.png"></a>
+
+        </div>
+    </ul>
+</div>
+
+<slot></slot>
+
+<!--<div class = "content">-->
+
+
+<!--    <div class = "slot">-->
+
+<!--        <slot></slot>-->
+
+<!--    </div>-->
+<!--</div>-->
+
+<style>
+.content {
+    display: flex;
+    margin: 1rem;
+    padding: 0;
+    gap: 1rem;
+}
+
+.navbar_ {
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+    /*padding: 1rem;*/
+    padding-inline: 1rem;
+    background-color: rgb(247,248,249);
+    border-bottom: var(--orange-yellow-crayola) solid 1px;
+}
+
+.navbar-links a {
+    color: black;
+}
+
+.navbar-item ul {
+    margin: 0;
+    padding: 0;
+}
+
+li {
+    list-style: none;
+}
+
+a {
+    text-decoration: none;
+}
+
+.menu {
+    display: flex;
+    gap: 1em;
+    font-size: 18px;
+}
+
+.menu li:hover {
+    background-color: var(--orange-yellow-crayola);
+    color: black;
+    border-radius: 5px;
+    transition: 0.3s ease;
+}
+
+.menu li { padding: 5px}
+
+input[type=checkbox]{
+
+    display: none;
+
+}
+
+.hamburger {
+    display: none;
+    font-size: 24px;
+    user-select: none;
+    color:white;
+}
+
+@media (max-width: 768px) {
+    .menu {
+        display: none;
+        position: absolute;
+        background-color: var(--bg-primary);
+        right: 0;
+        left: 0;
+        text-align: center;
+
+    }
+
+    .menu li:hover {
+        display: inline-block;
+        background-color: #6649b8;
+        transition: 0.3s ease;
+    }
+
+    input[type=checkbox]:checked ~ .menu {
+        display: block;
+    }
+
+    .hamburger {
+        display: block;
+        color:white;
+    }
+
+
+
+}
+
+</style>
+
+
---
+++ site/src/routes/contact/+page.svelte
@@ -0,0 +1,276 @@
+<section style = "display: flex; flex-direction: column">
+
+    <div class = "fill_screen bg_1 hero">
+
+        <div class = "hero_name">
+            <h2>Hello, my name is </h2>
+            <h1>Jordan Dialpuri</h1>
+        </div>
+
+        <img src = "images/memoji.png"/>
+
+    </div>
+
+    <div class = "bg_2 about" id="about">
+
+        <div class = "timeline">
+            <h3>Education</h3>
+            <ul>
+                <li>
+                    <span>October 2022 - Present</span>
+                    <div class="content">
+                        <div class = "text-content">
+
+                            <h3>PhD in Biological Chemistry - University of York</h3>
+
+                            <div class = "extra-content">
+                            <p>
+                                <span>Project Title: Automated model building of protein-nucleotide complexes using X-ray data and Alphafold models.</span>
+                                <span>Supervisors: Professor Kevin Cowtan and Dr Jon Agirre</span>
+                                <span>Presented work at Proteins by Candlelight conference in first month of PhD</span>
+                            </p>
+                            </div>
+                        </div>
+                    </div>
+                </li>
+
+                <li>
+                    <span>September 2018 - August 2022</span>
+                    <div class="content">
+                        <div class = "text-content">
+                            <h3>MChem in Chemistry - University of York</h3>
+                            <div class = "extra-content">
+
+                            <p>
+                                Graduated with First Class Honours with Distinction (1*).
+                            </p>
+                        </div>
+                    </div>                        </div>
+
+                </li>
+                <li>
+                    <span>September 2011 - June 2018</span>
+                    <div class="content">
+                        <div class = "text-content">
+                            <h3>King Edwards VI Camp Hill School for Boys</h3>
+                            <div class = "extra-content">
+
+                            <span>• A levels in Maths (A), Chemistry (A) and Biology (A)</span>
+                            <span>• AS level in Computer Science (A)</span>
+                            <span>• 10 GCSEs (5 A* and 5A)</span>
+
+                        </div>
+
+                    </div>                        </div>
+
+                </li>
+
+            </ul>
+        </div>
+        <div class = "timeline">
+            <h3>Experience</h3>
+
+            <ul>
+                <li>
+                    <span>October 2022 - Present</span>
+                    <div class="content">
+                        <div class = "text-content">
+
+                            <h3>Summer Project - University of York</h3>
+                            <div class = "extra-content">
+
+                            <p>
+                                <span>Project Title: N-glycan torsion angles</span>
+                                <span>Supervisors: Professor Kevin Cowtan and Dr Jon Agirre</span>
+                                <span>Presented work at Proteins by Candlelight conference in first month of PhD</span>
+                            </p>
+                        </div>                        </div>
+
+                    </div>
+                </li>
+                <li>
+                    <span>June 2021 - August 2022</span>
+                    <div class="content">
+                        <div class = "text-content">
+                            <h3>Industrial Placement Student - Afton Chemical</h3>
+                            <div class = "extra-content">
+
+                            <span>• One year placement as part of fourth year of undergraduate degree, undertaking a research
+                            project.</span>
+                            <span>• Developed novel experiments and quantified success by running frequent infrared spectroscopy
+                            analyses.</span>
+                            <span>• Implementing machine learning models to assist company function.</span>
+                        </div>                        </div>
+
+
+                    </div>
+                </li>
+                <li>
+                    <span>June 2018 - August 2022</span>
+                    <div class="content">
+                        <div class = "text-content">
+                            <h3>MChem in Chemistry - University of York</h3>
+                            <div class = "extra-content">
+
+                            <p>
+                                Graduated with First Class Honours with Distinction (1*).
+                            </p>
+                            </div>
+                        </div>
+                    </div>
+                </li>
+
+            </ul>
+
+
+        </div>
+
+    </div>
+
+    <div class = "fill_screen bg_3" id="projects">
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusamus adipisci animi, atque ipsa laudantium libero minus nulla. Ab aliquam aut, commodi delectus, dolor dolore eum labore magnam praesentium quia quisquam!
+    </div>
+
+</section>
+<style>
+
+    .bg_1 {
+        background-color: var(--bg-1);
+    }
+
+    .bg_2 {
+        background-color: var(--bg-2);
+    }
+
+    .bg_3 {
+        background-color: var(--bg-3);
+    }
+
+    .bg_4 {
+        background-color: var(--bg-4);
+    }
+
+    .bg_5 {
+        background-color: var(--bg-5);
+    }
+
+    .flex {
+        display: flex;
+    }
+
+    .about {
+        display: flex;
+        flex-direction: row;
+        justify-content: space-evenly;
+        align-items: center;
+        align-content: center;
+        /*flex-grow: 0;*/
+        min-height: 100vh;
+        width: 100%;
+    }
+
+    .hero_name {
+        display: flex;
+        flex-direction: column;
+    }
+
+    .hero_name h1 {
+        font-size: 5rem;
+    }
+
+    .hero {
+        display: flex;
+        flex-direction: row;
+        justify-content: space-evenly;
+        align-items: center;
+        justify-content: center;
+        /*flex-grow: inherit;*/
+        gap: 2rem;
+        color : white;
+    }
+
+    @media (max-width: 600px) {
+        .hero img { display: none}
+
+        .extra-content { display: none}
+        .about { flex-direction: column}
+    }
+    .hero img {
+        max-width: 30vh;
+        max-height: 30vh;
+        aspect-ratio: 1/1;
+    }
+
+    .fill_screen {
+        width: 100%;
+        height: 100vh;
+    }
+
+    .timeline {
+        /*width: 800px;*/
+        /*width: 40vw;*/
+        /*min-width: 30vw;*/
+        background-color: var(--bg-2);
+        color: #fff;
+        padding: 30px 20px;
+        margin-inline: 1rem;
+        /*box-shadow:0px 0px 10px rgba(0,0,0,.5);*/
+    }
+
+    .timeline ul {
+        list-style-type: none;
+        border-left: 2px solid var(--bg-5);
+        padding: 10px 5px;
+
+    }
+
+    .timeline ul li {
+        padding: 20px 20px;
+        position: relative;
+        cursor: pointer;
+        border-radius: 20px;
+        /*box-shadow: 0px 0px 5px var(--bg-5);*/
+        transition: .5s;
+        /*background-color: var(--bg-2);*/
+        margin-top: 1rem;
+        margin-bottom: 1rem;
+    }
+
+    .timeline ul li span {
+        display: inline-block;
+        background-color: var(--bg-2);
+        border-radius: 25px;
+        padding: 2px 5px;
+        font-size: 15px;
+        /*text-align: center;*/
+    }
+
+    .timeline ul li .content h3 {
+        color: var(--text-primary);
+        font-size: 17px;
+        padding-top: 5px;
+    }
+
+    .timeline ul li .content p {
+        padding: 5px 0px 15px 0px;
+        font-size: 15px;
+    }
+
+    .timeline ul li:before {
+        position: absolute;
+        content: '';
+        width: 10px;
+        height: 10px;
+        background-color: var(--bg-5);
+        border-radius: 50%;
+        left: -11px;
+        top: 28px;
+        transition: .5s;
+    }
+
+    .timeline ul li:hover:before {
+        background-color: var(--bg-5);
+        box-shadow: 0px 0px 10px 2px var(--bg-5);
+    }
+
+</style>
