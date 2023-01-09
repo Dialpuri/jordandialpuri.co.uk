ee25cf409868f29377640735da5bac28d251f2de
Dialpuri <dialpuri.jordan@gmail.com>
2023-01-09 23:32:14 +0000

    Updated build

--- site/vite.config.js
+++ site/vite.config.js
@@ -4,9 +4,9 @@ import { sveltekit } from '@sveltejs/kit/vite';
 const config = {
 	plugins: [sveltekit()], 
 	root: 'src', 
-	build: { 
-		outDir : "../../build"	
-	}
+	// build: {
+	// 	outDir : "../../build"
+	// }
 };
 
 export default config;
