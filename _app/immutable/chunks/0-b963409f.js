2b8f60b2c50300b4c41ffb1f1308f584636c3f3d
Dialpuri <dialpuri.jordan@gmail.com>
2023-01-09 23:33:13 +0000

    Moved build to root

---
+++ _app/immutable/chunks/0-b963409f.js
@@ -0,0 +1,1 @@
+import{_ as r}from"./_layout-76b2ab9d.js";import{default as t}from"../components/pages/_layout.svelte-6d8aceee.js";export{t as component,r as universal};
---
+++ _app/immutable/chunks/2-018f97f1.js
@@ -0,0 +1,1 @@
+import{default as t}from"../components/pages/_page.svelte-def90f9d.js";export{t as component};
