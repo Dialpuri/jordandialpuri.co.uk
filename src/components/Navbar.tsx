b201b31bfbfef9637c89ac7d6bd789da1a8a137f
Jordan Dialpuri <44945647+Dialpuri@users.noreply.github.com>
2024-12-21 15:30:26 +0000

    Fixed navbar clash on small screens

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -3,8 +3,14 @@ export default function Navbar() {
         <header className="bg-primary flex flex-row px-4 p-8 align-middle justify-center text-l md:text-xl lg:text-xl lg:px-24">
 
             <div className="flex-col mr-auto">
-                <h1 className=" font-extrabold leading-none tracking-tight text-gray-900 mt-auto mb-auto">Jordan Dialpuri</h1>
-                <span className="text-sm text-gray-500">PhD Student | University of York</span>
+                <h1 className=" font-extrabold leading-none tracking-tight text-gray-900 mt-auto mb-auto">Jordan
+                    Dialpuri</h1>
+                <div className="flex flex-col mt-1 sm:flex-row sm:space-x-1 ">
+                <span className="text-sm text-gray-500">PhD Student</span>
+                <span className="text-sm text-gray-500 hidden sm:block">|</span>
+                <span className="text-sm text-gray-500">University of York</span>
+                </div>
+
             </div>
             <nav className="flex flex-row space-x-6 mt-auto mb-auto">
                 <a href="#about" className="font-bold tracking-tight">About</a>
